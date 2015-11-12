ddd1895db7e0c17498f9b00ef7e1ab23d76cebb7
Rogers Guedes <rogerguedes.ft@gmail.com>
2015-11-12 00:46:54 -0500

    my server has no mod_rewrite =/

--- src/php/application/config/config.php
+++ src/php/application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '//rogers.noip.me/flowmeter';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = '';
+$config['index_page'] = 'index.php';
 
 /*
 |--------------------------------------------------------------------------
